b2a2fb35c2bb58d2264d4a605478babf011731f4
bulangxiong <45414734+bulangxiong@users.noreply.github.com>
2019-01-22 21:07:05 +0800

    gou

---
+++ app/src/main/java/movie/bw/com/movie/activity/RegiterActivity.java
@@ -0,0 +1,15 @@
+package movie.bw.com.movie.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import movie.bw.com.movie.R;
+
+public class RegiterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_regiter);
+    }
+}
