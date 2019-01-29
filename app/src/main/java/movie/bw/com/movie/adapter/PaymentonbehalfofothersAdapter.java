553fec13ad3300b2f7ba7c4b2fb574a807d5ffd4
bulangxiong <45414734+bulangxiong@users.noreply.github.com>
2019-01-29 16:36:14 +0800

    购票记录

---
+++ app/src/main/java/movie/bw/com/movie/adapter/PaymentonbehalfofothersAdapter.java
@@ -0,0 +1,98 @@
+package movie.bw.com.movie.adapter;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.bw.movie.R;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.List;
+
+import movie.bw.com.movie.bean.TheticketrecordBean;
+
+import static com.umeng.analytics.pro.k.a.b;
+import static com.umeng.analytics.pro.k.a.i;
+import static com.umeng.analytics.pro.k.a.s;
+
+/**
+ * Created by zxk
+ * on 2019/1/29 14:27
+ * QQ:666666
+ * Describe:
+ */
+public class PaymentonbehalfofothersAdapter extends RecyclerView.Adapter<PaymentonbehalfofothersAdapter.VH> {
+    private List<TheticketrecordBean> list=new ArrayList<>();
+    private Context context;
+
+    public PaymentonbehalfofothersAdapter(Context context) {
+        this.context = context;
+    }
+    public void addItem(List<TheticketrecordBean> beans){
+        if (beans!=null){
+            list.addAll(beans);
+        }
+    }
+    @NonNull
+    @Override
+    public VH onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        View view = LayoutInflater.from(context).inflate(R.layout.payment_item, viewGroup, false);
+
+        return new VH(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull VH vh, int i) {
+        TheticketrecordBean bean = list.get(i);
+        vh.name.setText(bean.getMovieName());
+        vh.odd_hao.setText("订单号："+bean.getOrderId());
+        SimpleDateFormat format1 = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        String format = format1.format(bean.getCreateTime());
+        vh.cinem.setText("影院："+bean.getCinemaName());
+        vh.th_film_office.setText("影厅："+bean.getScreeningHall());
+        vh.timea.setText("时间："+format);
+        vh.number.setText("数量："+bean.getAmount()+"张");
+        vh.money.setText("金额："+bean.getPrice()+"元");
+//        SimpleDateFormat format2 = new SimpleDateFormat("HH:mm:ss");
+//        String begin = format2.format(bean.getBeginTime());
+//        String end = format2.format(bean.getEndTime());
+//        long deadline = Long.parseLong(format);
+
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return list.size();
+    }
+
+    class VH extends RecyclerView.ViewHolder{
+
+        private final Button go_buymoney;
+        private final TextView odd_hao;
+        private final TextView cinem;
+        private final TextView name;
+        private final TextView timea;
+        private final TextView number;
+        private final TextView money;
+        private final TextView th_film_office;
+
+        public VH(@NonNull View itemView) {
+            super(itemView);
+            name = itemView.findViewById(R.id.name);
+            go_buymoney = itemView.findViewById(R.id.go_buymoney);
+            odd_hao = itemView.findViewById(R.id.odd_hao);
+            cinem = itemView.findViewById(R.id.cinem);
+            timea = itemView.findViewById(R.id.timea);
+            number = itemView.findViewById(R.id.number);
+            money = itemView.findViewById(R.id.money);
+            th_film_office = itemView.findViewById(R.id.th_film_Office);
+        }
+    }
+}
